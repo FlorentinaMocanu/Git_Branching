fb9cf89e77551124fcde778365ff15eb9e92c803
Florentina Mocanu <florypopescu25@gmail.com>
2022-12-13 22:10:20 +0000

    change to TC001

--- src/testCase/TC001.java
+++ src/testCase/TC001.java
@@ -5,9 +5,11 @@ public class TC001 {
     public static void main(String[] args) {
 
 
-        //testing somr things
+        //testing some things
         //more testing
         //final test
+
+        //negative testing
     }
 
 }
